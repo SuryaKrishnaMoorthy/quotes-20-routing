5d655da17c711e0b770b5a12cb9d228777ad5a31
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2021-11-08 16:22:47 -0500

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
+import React, { Suspense} from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
-import NotFound from './pages/NotFound';
+// import AllQuotes from './pages/AllQuotes';
+// import NewQuote from './pages/NewQuote';
+// import QuoteDetail from './pages/QuoteDetail';
+// import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 import Layout from './components/layout/Layout';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const AllQuotes = React.lazy(() => import('./pages/AllQuotes'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path='/' element={<Navigate replace to='/quotes' />} /> {/* redirect */}
-        <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
-        <Route path='/quotes' element={<AllQuotes />} />
-        <Route path='/new-quote' element={<NewQuote />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Layout>
+    <Suspense 
+      fallback={
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>
+      }
+    >
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Navigate replace to='/quotes' />} /> {/* redirect */}
+          <Route path='/quotes/:quoteId/*' element={<QuoteDetail />} />
+          <Route path='/quotes' element={<AllQuotes />} />
+          <Route path='/new-quote' element={<NewQuote />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </Suspense>
   );
 }
 
